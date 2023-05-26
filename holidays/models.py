4fea921620cb1145af949946fbda6bbe939722dd
adrianleung5 <adrianleung50@gmail.com>
2023-05-26 15:56:48 +0000

    finnish holidays model

--- holidays/models.py
+++ holidays/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import user
+
+from djrichtextfeld.models import RichTextField
 
 # Create your models here.
+
+Class Holidays(models.Model):
+
+    user = models.Foreignkey(User, related_name='receipe owner,' on_delete=models.CASCADE)
+    title = models.Charfield(max_length=300, null =False, blank=False)
+    description = models.Charfield(max_length=500, null=False, blank=False)
+    Instructions = RichTextField (max_length=10000, null=False, blank=False)
+    Highlights = RichTextField(max_length=10000, null=False, blank=False)
+    image = ResizedImageField(
+        size={400, None}, quality=75, upload_to='recipes/', force_format='WEBP'
+        blank=False, null=False 
+    ) 
+
