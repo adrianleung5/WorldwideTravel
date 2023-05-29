c851b3c2582df94d9e1b85006c7b35893976465c
adrianleung5 <adrianleung50@gmail.com>
2023-05-29 22:27:24 +0000

    fix admin,py file

--- holidays/admin.py
+++ holidays/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Holidays
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 admin.site.register(Holidays)
+
+@admin.register(Holidays)
+class HolidaysAdnub(SummerNoteModelAdmin):
+    summernote_fields = ('content')
--- worldwidetravel/urls.py
+++ worldwidetravel/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('djrichtextfield/', include('djrichtextfield.urls')),
+    path('summernote', include('django_summernote.urls')),
 ]
