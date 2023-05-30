361c5e7dea605f2e9dbad77dbd3f86c0c97cc4b8
adrianleung5 <adrianleung50@gmail.com>
2023-05-30 17:08:03 +0000

    add comments model

--- holidays/admin.py
+++ holidays/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Holidays
+from .models import comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -11,4 +12,11 @@ class HolidaysAdnub(SummernoteModelAdmin):
     list_filter = ('status', )
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
-    
+
+@admin.register(Comment)
+class AdminForComment(admin.ModelAdmin):
+    """This is for the admin to manage Comments"""
+    list_filter = ('date_added',)
+    list_display = ('name', 'info', 'date_added')
+    search_fields = ('name', 'email', 'info')
+
